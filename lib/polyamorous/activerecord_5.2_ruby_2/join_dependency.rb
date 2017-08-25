3f9fd5578a87d4ed3f01d9d4a2282d3711db8069
Yvan BARTHÉLEMY <yvan@capsens.eu>
2017-08-25 16:50:36 +0200

    Remove functionality as it breaks globalize

--- lib/polyamorous/activerecord_5.2_ruby_2/join_dependency.rb
+++ lib/polyamorous/activerecord_5.2_ruby_2/join_dependency.rb
@@ -5,29 +5,30 @@ module Polyamorous
     # Replaces ActiveRecord::Associations::JoinDependency#build
     #
     def build(associations, base_klass)
-      associations.map do |name, right|
-        if name.is_a? Join
-          reflection = find_reflection base_klass, name.name
-          reflection.check_validity!
-          reflection.check_eager_loadable! if ActiveRecord::VERSION::MAJOR >= 5
-
-          klass = if reflection.polymorphic?
-            name.klass || base_klass
-          else
-            reflection.klass
-          end
-          JoinAssociation.new(reflection, build(right, klass), name.klass, name.type)
-        else
-          reflection = find_reflection base_klass, name
-          reflection.check_validity!
-          reflection.check_eager_loadable! if ActiveRecord::VERSION::MAJOR >= 5
-
-          if reflection.polymorphic?
-            raise ActiveRecord::EagerLoadPolymorphicError.new(reflection)
-          end
-          JoinAssociation.new reflection, build(right, reflection.klass)
-        end
-      end
+      # associations.map do |name, right|
+      #   if name.is_a? Join
+      #     reflection = find_reflection base_klass, name.name
+      #     reflection.check_validity!
+      #     reflection.check_eager_loadable! if ActiveRecord::VERSION::MAJOR >= 5
+      #
+      #     klass = if reflection.polymorphic?
+      #       name.klass || base_klass
+      #     else
+      #       reflection.klass
+      #     end
+      #     JoinAssociation.new(reflection, build(right, klass), name.klass, name.type)
+      #   else
+      #     reflection = find_reflection base_klass, name
+      #     reflection.check_validity!
+      #     reflection.check_eager_loadable! if ActiveRecord::VERSION::MAJOR >= 5
+      #
+      #     if reflection.polymorphic?
+      #       raise ActiveRecord::EagerLoadPolymorphicError.new(reflection)
+      #     end
+      #     JoinAssociation.new reflection, build(right, reflection.klass)
+      #   end
+      # end
+      super
     end
 
     def find_join_association_respecting_polymorphism(reflection, parent, klass)
@@ -56,23 +57,24 @@ module Polyamorous
     # passing an additional argument, `join_type`, to #join_constraints.
     #
     def join_constraints(outer_joins, join_type)
-      joins = join_root.children.flat_map { |child|
-        if join_type == Arel::Nodes::OuterJoin
-          make_polyamorous_left_outer_joins join_root, child
-        else
-          make_polyamorous_inner_joins join_root, child
-        end
-      }
-
-      joins.concat outer_joins.flat_map { |oj|
-        if join_root.match? oj.join_root
-          walk(join_root, oj.join_root)
-        else
-          oj.join_root.children.flat_map { |child|
-            make_outer_joins(oj.join_root, child)
-          }
-        end
-      }
+      # joins = join_root.children.flat_map { |child|
+      #   if join_type == Arel::Nodes::OuterJoin
+      #     make_polyamorous_left_outer_joins join_root, child
+      #   else
+      #     make_polyamorous_inner_joins join_root, child
+      #   end
+      # }
+      #
+      # joins.concat outer_joins.flat_map { |oj|
+      #   if join_root.match? oj.join_root
+      #     walk(join_root, oj.join_root)
+      #   else
+      #     oj.join_root.children.flat_map { |child|
+      #       make_outer_joins(oj.join_root, child)
+      #     }
+      #   end
+      # }
+      super
     end
 
     # Replaces ActiveRecord::Associations::JoinDependency#make_left_outer_joins,
@@ -80,25 +82,27 @@ module Polyamorous
     # https://github.com/rails/rails/commit/e038975
     #
     def make_polyamorous_left_outer_joins(parent, child)
-      tables    = child.tables
-      join_type = Arel::Nodes::OuterJoin
-      info      = make_constraints parent, child, tables, join_type
-
-      [info] + child.children.flat_map { |c|
-        make_polyamorous_left_outer_joins(child, c)
-      }
+      # tables    = child.tables
+      # join_type = Arel::Nodes::OuterJoin
+      # info      = make_constraints parent, child, tables, join_type
+      #
+      # [info] + child.children.flat_map { |c|
+      #   make_polyamorous_left_outer_joins(child, c)
+      # }
+      super
     end
 
     # Replaces ActiveRecord::Associations::JoinDependency#make_inner_joins
     #
     def make_polyamorous_inner_joins(parent, child)
-      tables    = child.tables
-      join_type = child.join_type || Arel::Nodes::InnerJoin
-      info      = make_constraints parent, child, tables, join_type
-
-      [info] + child.children.flat_map { |c|
-        make_polyamorous_inner_joins(child, c)
-      }
+      # tables    = child.tables
+      # join_type = child.join_type || Arel::Nodes::InnerJoin
+      # info      = make_constraints parent, child, tables, join_type
+      #
+      # [info] + child.children.flat_map { |c|
+      #   make_polyamorous_inner_joins(child, c)
+      # }
+      super
     end
 
     private :make_polyamorous_inner_joins, :make_polyamorous_left_outer_joins
@@ -107,24 +111,24 @@ module Polyamorous
       # Prepended before ActiveRecord::Associations::JoinDependency#walk_tree
       #
       def walk_tree(associations, hash)
-        case associations
-        when TreeNode
-          associations.add_to_tree(hash)
-        when Hash
-          associations.each do |k, v|
-            cache =
-              if TreeNode === k
-                k.add_to_tree(hash)
-              else
-                hash[k] ||= {}
-              end
-            walk_tree(v, cache)
-          end
-        else
-          super(associations, hash)
-        end
+        # case associations
+        # when TreeNode
+        #   associations.add_to_tree(hash)
+        # when Hash
+        #   associations.each do |k, v|
+        #     cache =
+        #       if TreeNode === k
+        #         k.add_to_tree(hash)
+        #       else
+        #         hash[k] ||= {}
+        #       end
+        #     walk_tree(v, cache)
+        #   end
+        # else
+        #   super(associations, hash)
+        # end
+        super
       end
     end
-
   end
 end
